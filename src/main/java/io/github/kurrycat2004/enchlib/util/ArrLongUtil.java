d277bf0fb8b618d0329f3d5054f961915f9dc1d4
kurrycat <kurrycat2004+github@gmail.com>
2025-06-21 18:42:10 +0200

    fix string representation for numbers <2^64

--- src/main/java/io/github/kurrycat2004/enchlib/util/ArrLongUtil.java
+++ src/main/java/io/github/kurrycat2004/enchlib/util/ArrLongUtil.java
@@ -12,14 +12,15 @@ public class ArrLongUtil {
     public static String toZillion(ArrLong arrLong) {
         final long[] data = arrLong.data();
         int highestBit = arrLong.highestBit();
-        if (highestBit <= Long.SIZE && Long.compareUnsigned(data[0], 1_000_000) < 0) {
+        if (highestBit < 64 && Long.compareUnsigned(data[0], 1_000_000) < 0) {
             if (data[0] < 1_000) return Long.toString(data[0]);
-            return String.format(Locale.US, "%.2f", data[0] / 1_000.0) + " thousand";
+            return String.format(Locale.US, "%.2f thousand", data[0] / 1_000.0);
         }
 
-        long word = arrLong.unalignedULongTo(highestBit);
+        int startBit = highestBit < 64 ? 0 : highestBit - 63;
+        long word = arrLong.unalignedULongFrom(startBit);
         double value = MathUtil.uLongToDouble(word);
-        double nLog10 = Math.log10(value) + highestBit * LOG10_2;
+        double nLog10 = Math.log10(value) + startBit * LOG10_2;
 
         int N = (int) Math.floor(nLog10 / 3.0);
 
